0e1073c8af1efe4467e2df1cd311baa6e69c62eb
tuvshinbat <tuvshinbat596@gmail.com>
2022-06-01 09:47:07 +0800

    first commit

--- axios-base.js
+++ axios-base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://beta.queenbella.mn/rest/api/v1/",
-  baseURL: "http://localhost:8022/api/v1/",
+  baseURL: "http://ac.lvg.mn/rest/api/v1/",
 });
 
 instance.defaults.withCredentials = true;
