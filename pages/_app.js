8b6b06f81e11f2426e60ec2737ee5f524cf2dd54
Tubshinbat <tuvshinbat596@gmail.com>
2022-06-09 15:17:46 +0800

    google

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ const WOW = !isServer ? require("wow.js") : null;
 import Router from "next/router";
 import Nprogress from "nprogress";
 import { CookiesProvider } from "react-cookie";
+import * as gtag from "lib/gtag";
+import { useRouter } from "next/router";
 
 import "nprogress/nprogress.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -32,6 +34,19 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     new WOW().init();
   }, []);
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   const fetcher = async (url) => {
     const res = await fetch(url);
@@ -86,15 +101,24 @@ function MyApp({ Component, pageProps }) {
             src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
             crossorigin
           ></Script>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-B89PWLS9QM"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-B89PWLS9QM');
-          </script>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <CookiesProvider>
           <Component {...pageProps} />
