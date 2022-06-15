d5a64f2bf0c3082aebee5807a24ba8c24ef1f039
Janchiw <d.janchiw@gmail.com>
2022-06-15 22:28:50 +0800

    a

--- axios-base.js
+++ axios-base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://beta.queenbella.mn/rest/api/v1/",
-  baseURL: "https://ac-admin.lvg.mn/rest/api/v1/",
+  baseURL: "https://admin.khuduugaray.com/rest/api/v1/",
 });
 
 instance.defaults.withCredentials = true;
